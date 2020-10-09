4793cebc2c90ff681b14dd55e3861a0029c4dc86
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-09 11:48:38 -0700

    search for books

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import{useMutation} from '@apollo/react-hooks';
+import {SAVE_BOOK} from '../utils/mutations';
+
+
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -52,6 +56,8 @@ const SearchBooks = () => {
     }
   };
 
+  const [saveBook,{error}] = useMutation(SAVE_BOOK)
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -65,11 +71,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({
+        variables: {bookToSave}
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+// get me
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
