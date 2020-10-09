aa6a115fb3bf4066a5915afe50c7b41b9fd6d839
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-09 16:46:26 -0700

    fixed delete books bug

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,21 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from '../utils/mutations';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   
+  const { loading, data} = useQuery(GET_ME,{
+    update(cache,{data}){
+      try{
+        const{me}=cache.readQuery({query:GET_ME});
+        cache.writeQuery({
+          query:GET_ME,
+          data:{me:[data,...me]}
+        })
+    } catch(e){
+      console.log(e)
+    }
+  }
+  });
 
-  const { loading, data} = useQuery(GET_ME);
-  const userData = data?.me || {}
+  const userData = data?.me || [];
   const userDataLength = Object.keys(userData).length;
 
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK,{
+    update(cache,{data:{removeBook}})
+    {
+      try{
+        const{me}=cache.readQuery({query: GET_ME});
+        cache.writeQuery({
+          query:GET_ME,
+          data:{me:[removeBook, ...me]}
+        });
+      } catch(e){
+        console.log(e);
+      }
+    }
+  });
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -39,7 +63,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
-import{useMutation} from '@apollo/react-hooks';
+import {useMutation} from '@apollo/react-hooks';
 import {SAVE_BOOK} from '../utils/mutations';
-
+import{GET_ME} from '../utils/queries';
 
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
@@ -17,6 +16,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook,{error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -56,13 +57,12 @@ const SearchBooks = () => {
     }
   };
 
-  const [saveBook,{error}] = useMutation(SAVE_BOOK);
-
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
     const{title,authors,description, image,link} = bookToSave;
+
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -74,6 +74,8 @@ const SearchBooks = () => {
       await saveBook({
         variables: {title,authors,description,image,link,bookId:bookToSave.bookId}
       });
+
+   
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
