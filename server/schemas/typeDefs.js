b2e219b1361355f808520309980f2b75e0001af2
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-08 23:35:21 -0700

    backend

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+type User {
+    _id:ID
+    username:String
+    email:String
+    password:String
+    bookCount:Int 
+    savedBooks:[Book]
+}
+
+type Book {
+    bookId:String
+    authors: [String]
+    description:String
+    title:String
+    image:String
+    link:String
+}
+type Query{
+    me:User
+    users:[User]
+}
+
+type Mutation{
+    login(email:String!, password:String!):Auth
+    addUser(username:String,email:String,password:String):Auth
+    saveBook(authors:[String], description:String, title:String, BookId:String, image:String, link:String):User
+    removeBook(bookId:ID!):User
+}
+
+type Auth {
+    token:ID
+    user:User
+}
+`;
+
+module.exports = typeDefs;
